f1ae70ed6469e6eb2285285b58cb86a458c083c3
shijinhua <shijinhua@xiaoduotech.com>
2022-03-07 23:44:20 +0800

    merge

---
+++ install.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+# Copyright 2019 the rust-swc authors. All rights reserved. MIT license.
+# TODO(everyone): Keep this script simple and easily auditable.
+
+set -e
+
+if ! command -pVv tar >/dev/null; then
+	echo "Error: tar is required to install rust-swc." 1>&2
+	exit 1
+fi
+
+if [ "$OS" = "Windows_NT" ]; then
+	target="windows"
+else
+	case $(uname -sm) in
+	"Darwin x86_64") target="macos" ;;
+	"Darwin arm64") target="macos" ;;
+	*) target="linux" ;;
+	esac
+fi
+
+deno_uri="https://github.com/mynane/rust-swc/releases/download/v1.0.4/rust-swc-${target}.tar.gz"
+
+deno_install="${DENO_INSTALL:-$HOME/.rust-swc}"
+
+bin_dir="$deno_install"
+exe="$bin_dir/rust-swc"
+
+if [ ! -d "$bin_dir" ]; then
+	mkdir -p "$bin_dir"
+fi
+curl --fail --location --progress-bar --output "$exe.tar.gz" "$deno_uri"
+tar -zxvf "$exe.tar.gz" -C "$bin_dir"
+chmod +x "$exe"
+rm "$exe.tar.gz"
+
+echo "rust-swc was installed successfully to $exe"
+if command -v rust-swc >/dev/null; then
+	echo "Run 'rust-swc --help' to get started"
+else
+	case $SHELL in
+	/bin/zsh) shell_profile=".zshrc" ;;
+	*) shell_profile=".bash_profile" ;;
+	esac
+	echo "Manually add the directory to your \$HOME/$shell_profile (or similar)"
+	echo "  export RUST_SWC_INSTALL=\"$deno_install\""
+	echo "  export PATH=\"\$RUST_SWC_INSTALL:\$PATH\""
+	echo "Run '$exe --help' to get started"
+fi
